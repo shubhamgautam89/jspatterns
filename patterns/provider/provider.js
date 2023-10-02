9f35d765cfd6bc7675d49ee1bee99bc0308243db
shubhamgautam <shubhcool1@gmail.com>
2023-10-02 17:56:14 +0530

    provider pattern example

--- patterns/provider/provider.js
+++ patterns/provider/provider.js
@@ -2,7 +2,13 @@
  * provider pattern is very specific to react.
  * instead of prop drilling when one prop is passed to nested children
  * A context-provider is cleaner way to pass props to children
- */
+ 
+    ** Pros: **
+        - The Provider pattern/Context API makes it possible to pass data to many components, without having to manually pass it through each component layer.
+
+    ** Cons: **
+        - In some cases, overusing the Provider pattern can result in performance issues. All components that consume the context re-render on each state change.
+*/
 import React, { useContext, useState } from "react";
 
 const themes = {};
